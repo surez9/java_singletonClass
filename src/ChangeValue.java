cf33b487f7e68c0df23ff8666d0510f2ddf5dfdb
sureshbhandari <sd3vil9@gmail.com>
2024-12-30 14:15:14 +0545

    Change in any variable of Singleton class changes other variable

--- src/ChangeValue.java
+++ src/ChangeValue.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class ChangeValue {
+//Main Class
+public class ChangeValue {
+    //Main driver method
+    public static void main(String[] args) {
+        // Instantiating Singleton class with variable x
+        Singleton x = Singleton.getInstance();
+
+        // Instantiating Singleton class with variable y
+        Singleton y = Singleton.getInstance();
+
+        // Instantiating Singleton class with variable z
+        Singleton z = Singleton.getInstance();
+
+        // Now  changing variable of instance x
+        // via toUpperCase() method
+        x.s = (x.s).toUpperCase();
+
+        // Print and display commands
+        System.out.println("String from x is " + x.s);
+        System.out.println("String from y is " + y.s);
+        System.out.println("String from z is " + z.s);
+        System.out.println("\n");
+
+        // Now again changing variable of instance z
+        z.s = (z.s).toLowerCase();
+
+        System.out.println("String from x is " + x.s);
+        System.out.println("String from y is " + y.s);
+        System.out.println("String from z is " + z.s);
+    }
 }
